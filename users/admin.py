e75d39cfe6988dc8f7a681868a4a95f4c556d335
Jacob Bexten <jacob.bexten@umontana.edu>
2020-10-09 19:56:45 -0600

    initial commit with users app and timeclock app created. field in user sign in to determine if staff or not. still need to figure out how to create user identification number.

---
+++ users/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+
+# Register your models here.
+
+from django.contrib.auth.admin import UserAdmin
+
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+
+from .models import CustomUser
+
+
+class CustomUserAdmin(UserAdmin):
+
+    add_form = CustomUserCreationForm
+
+    form = CustomUserChangeForm
+
+    model = CustomUser
+
+admin.site.register(CustomUser, CustomUserAdmin)
